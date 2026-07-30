6bf36d4f25e48a950cfc8f2ae163ce8c308dae1c
alexiboat <boatengalex18@gmail.com>
2026-07-30 11:36:51 +0000

    vite allowedHost updated

--- Admin/vite.config.js
+++ Admin/vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  preview:{
-    allowedHosts:['ecommerceapp-o3n74.sevalla.app']
+  server: {
+    allowedHosts: true
+  },
+  preview: {
+    allowedHosts: true
   }
 })
+
