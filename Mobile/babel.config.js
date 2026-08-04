0d6e63fb0bc046af254058e9587e647197f2fe93
alexiboat <boatengalex18@gmail.com>
2026-08-04 20:51:01 +0000

    babel updated

--- Mobile/babel.config.js
+++ Mobile/babel.config.js
@@ -1,3 +1,4 @@
+
 module.exports = function (api) {
   api.cache(true);
   return {
@@ -5,8 +6,23 @@ module.exports = function (api) {
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
+    plugins: [
+      "react-native-reanimated/plugin",
+    ],
   };
 };
+
+
+
+// module.exports = function (api) {
+//   api.cache(true);
+//   return {
+//     presets: [
+//       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+//       "nativewind/babel",
+//     ],
+//   };
+// };
 
 // module.exports = function (api) {
 //   api.cache(true);
