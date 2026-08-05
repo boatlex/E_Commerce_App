6957d756c9216136eb55ab03bc477abd2c10462f
alexiboat <boatengalex18@gmail.com>
2026-08-05 02:21:10 +0000

    small fixes to app.json

--- Mobile/tailwind.config.js
+++ Mobile/tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
@@ -11,26 +10,25 @@ module.exports = {
           light:"#1ED760",
           dark:"#1AA34A"
         },
-         background:{
+        background:{
           DEFAULT:"#121212",
           light:"#181818",
           lighter:"#282828"
         },
-         surface:{
+        surface:{
           DEFAULT:"#282828",
           light:"#3E3E3E",
         },
-         text:{
+        text:{
           primary:"#ffffff",
           secondary:"#E5E5E5",
           tertiary:"#6A6A6A",
         },
-         accen:{
+        accent:{ // Fixed typo here (added 't')
           DEFAULT:"#1DB954",
           red:"#F44336",
           yellow:"#FFC107",
         },
-
       }
     },
   },
