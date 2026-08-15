cc2be9609425e7eb6cca4ea8edeb40b842dca30f
alexiboat <boatengalex18@gmail.com>
2026-08-15 01:09:31 +0000

    useCart and Cart screen updated

--- Mobile/app/(tabs)/cart.tsx
+++ Mobile/app/(tabs)/cart.tsx
@@ -1,8 +1,126 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Alert, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
 import SafeScreen from '../components/SafeScreen'
+import { WebView } from "react-native-webview"
+import useCart from '@/hooks/useCart'
+import { useAddresses } from '@/hooks/useAddresses'
+import { useApi } from '@/lib/api'
 
 const CartScreen = () => {
+  const api = useApi()
+  const {
+    addToCart,
+    isAddingToCart,
+    cart,
+    cartItemCount,
+    cartTotal,
+    clearCart,
+    isClearingCart,
+    isError,
+    isLoading,
+    isRemovingFromCart,
+    isUpdatingQuantity,
+    removeFromCart,
+    updateQuantity,
+  } = useCart()
+  const { addresses, } = useAddresses()
+
+  const [paymentLoading, setPaymentLoading] = useState(false)
+  const [checoutUrl, setCheckoutUrl] = useState(null)
+  const [addressModalVisible, setAddressModalVisible] = useState(false)
+
+
+  const cartItems = cart?.items || []
+  const subTotal = cartTotal
+  const shippingFee = 10.0
+  const tax = subTotal * 0.08
+  const total = subTotal + shippingFee + tax
+
+
+
+  const handleQuantityChange = (productId: string, currentQty: number, change: number) => {
+
+    const newQuantity = currentQty + change
+    if (newQuantity < 1) return
+
+    updateQuantity({ productId, quantity: newQuantity })
+  }
+
+  const handleRemoveItem = (productId: string, productName: string) => {
+    Alert.alert("Remove Item", ` Remove ${productName} from cart?`, [
+      { text: "Cancel", style: "cancel" },
+      {
+        text: "Remove", style: "destructive",
+        onPress: () => removeFromCart(productId)
+      }
+    ])
+  }
+
+  const handleChekout = () => {
+    if (cartItems.length === 0) return
+
+    if (!addresses || addresses.length === 0) {
+      Alert.alert("No Address", "Please provide shipping address before checking out",
+        [{ text: "Ok" }])
+      return
+    }
+
+    setAddressModalVisible(true)
+  }
+
+
+  // const startPayment = async () => {
+  //     setLoading(true);
+  //     try {
+  //       const response = await axios.post(BACKEND_URL, {
+  //         email: 'user@example.com',
+  //         amount: 50, // Value in GHS or NGN
+  //       });
+
+  //       if (response.data.status) {
+  //         setCheckoutUrl(response.data.data.authorization_url);
+  //       }
+  //     } catch (error) {
+  //       console.error('Payment initialization failed', error);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
+
+  //   // Monitor WebView navigation to detect completion or cancellation
+  //   const handleWebViewStateChange = (navState) => {
+  //     const { url } = navState;
+
+  //     if (url.includes('payment-success') || url.includes('checkout/thankyou')) {
+  //       setCheckoutUrl(null);
+  //       alert('Payment initialization finished! Processing your order.');
+  //     }
+  //   };
+
+  //   if (loading) {
+  //     return <View style={styles.center}><ActivityIndicator size="large" /></View>;
+  //   }
+
+  //   if (checkoutUrl) {
+  //     return (
+  //       <SafeAreaView style={styles.container}>
+  //         <WebView 
+  //           source={{ uri: checkoutUrl }} 
+  //           onNavigationStateChange={handleWebViewStateChange}
+  //           javaScriptEnabled={true}
+  //           domStorageEnabled={true}
+  //         />
+  //       </SafeAreaView>
+  //     );
+  //   }
+
+  //   return (
+  //     <View style={styles.center}>
+  //       <Button title="Pay Now" onPress={startPayment} />
+  //     </View>
+  //   );
+  // }
+
   return (
     <SafeScreen>
       <Text className='text-white'>CartScreen</Text>
--- Mobile/hooks/useCart.ts
+++ Mobile/hooks/useCart.ts
@@ -2,29 +2,87 @@ import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { useApi } from '@/lib/api'
 import { Cart } from '@/types'
 
-const useCart = ()=>{
+const useCart = () => {
 
 
-const queryClient = useQueryClient()
+   const queryClient = useQueryClient()
 
-const api = useApi()
- 
-const addToCartMutation = useMutation({
-     mutationFn: async ({productId, quantity=1}:{productId:string, quantity?:number})=>{
-       const {data} = await api.post<{cart:Cart}>("/carts", {productId, quantity})
-       return data.cart
-     },
+   const api = useApi()
 
-     onSuccess:()=>{
-      queryClient.invalidateQueries({queryKey:['cart']})
-     },
-    
- })
+   const { data: cart, isError, isLoading } = useQuery({
+      queryKey: ['cart'],
+      queryFn: async () => {
+         const { data } = await api.get<{ cart: Cart }>("/cart")
+         return data.cart
+      }
+   })
 
-    return{
-       addToCart:addToCartMutation.mutate,
-       isAddingToCart:addToCartMutation.isPending?addToCartMutation.variables.productId:null 
-    }
+
+   const addToCartMutation = useMutation({
+      mutationFn: async ({ productId, quantity = 1 }: { productId: string, quantity?: number }) => {
+         const { data } = await api.post<{ cart: Cart }>("/carts", { productId, quantity })
+         return data.cart
+      },
+
+      onSuccess: () => {
+         queryClient.invalidateQueries({ queryKey: ['cart'] })
+      },
+
+   })
+   const updateCartQuantityMutation = useMutation({
+      mutationFn: async ({ productId, quantity }: { productId: string, quantity: number }) => {
+         const { data } = await api.put<{ cart: Cart }>(`/carts/${productId}`, { quantity })
+         return data.cart
+      },
+
+      onSuccess: () => {
+         queryClient.invalidateQueries({ queryKey: ['cart'] })
+      },
+
+   })
+   const removeFromCartMutation = useMutation({
+      mutationFn: async (productId: string) => {
+         const { data } = await api.delete<{ cart: Cart }>(`/carts/${productId}` )
+         return data.cart
+      },
+
+      onSuccess: () => {
+         queryClient.invalidateQueries({ queryKey: ['cart'] })
+      },
+
+   })
+   const clearCartMutation = useMutation({
+      mutationFn: async () => {
+         const { data } = await api.delete<{ cart: Cart }>("/carts" )
+         return data.cart
+      },
+
+      onSuccess: () => {
+         queryClient.invalidateQueries({ queryKey: ['cart'] })
+      },
+
+   })
+
+   const cartTotal = cart?.items.reduce((sum, item)=> 
+      sum + item.product.price * item.quantity, 0) ?? 0
+
+   const cartItemCount = cart?.items.reduce((sum, item)=> sum + item.quantity,0)?? 0
+
+   return {
+      cart,
+      isError,
+      isLoading,
+      cartTotal,
+      cartItemCount,
+      updateQuantity:updateCartQuantityMutation.mutate,
+      removeFromCart:removeFromCartMutation.mutate,
+      clearCart:clearCartMutation.mutate,
+      addToCart: addToCartMutation.mutateAsync,
+      isAddingToCart: addToCartMutation.isPending ? addToCartMutation.variables.productId : null,
+      isRemovingFromCart: removeFromCartMutation.isPending ? removeFromCartMutation.variables: null,
+      isClearingCart:clearCartMutation.isPending,
+      isUpdatingQuantity:updateCartQuantityMutation.isPending
+   }
 }
 
 export default useCart
